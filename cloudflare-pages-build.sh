56c3f006cbd0b6fb556315f758230fad582228cf
jaipracash <pjai2909an@gmail.com>
2025-01-01 01:10:09 +0530

    Fix Flutter cloning in build script

--- cloudflare-pages-build.sh
+++ cloudflare-pages-build.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-# Install Flutter
-git clone https://github.com/flutter/flutter.git --branch stable --depth 1 flutter
-export PATH="$PWD/flutter/bin:$PATH"
+# Check if Flutter is already cloned, if not, clone it
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git --branch stable --depth 1 flutter
+fi
 
-# Ensure Flutter is in the path
+# Set Flutter environment variable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Ensure Flutter dependencies are installed
 flutter doctor
 
-# Build the web app
+# Build the Flutter web application
 flutter build web
